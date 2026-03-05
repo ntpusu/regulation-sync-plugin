782eb96ed19b2449ce5cc22de3e1271713574809
312432 <andy.wu.cyut.edu@gmail.com>
2026-03-05 20:37:33 +0800

    dependencies: setting up update checker

--- ntpusu-regulation-sync.php
+++ ntpusu-regulation-sync.php
@@ -1,8 +1,9 @@
 <?php
 /**
  * Plugin Name: NTPUSU Regulation Sync
+ * Plugin URI:  https://github.com/ntpusu/regulation-sync-plugin
  * Description: Fetches the latest regulation HTML from cloud.ntpusu.org and exposes it via a shortcode so editors can update a section with one click.
- * Version:     1.0.0
+ * Version:     1.0.0-rc1
  * Author:      AnJen Wu with help of Codex
  * License:     GPL-2.0-or-later
  *
@@ -22,3 +23,17 @@ require_once NTPUSU_REGULATION_SYNC_PATH . 'includes/admin.php';
 require_once NTPUSU_REGULATION_SYNC_PATH . 'includes/shortcode.php';
 
 add_action( NTPUSU_REGULATION_SYNC_CRON_HOOK, 'ntpusu_regulation_sync_run_cron' );
+
+// plugin-update-checker
+require_once __DIR__ . '/plugin-update-checker/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$updateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/ntpusu/regulation-sync-plugin/',
+    __FILE__,
+    'ntpusu_regulation_sync'
+);
+
+$updateChecker->setBranch('release');
+$updateChecker->getVcsApi()->enableReleaseAssets();
